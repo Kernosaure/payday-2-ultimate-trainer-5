b3348b4f841c54f6a4f30e71da2dc7edacd1ea4a
Kernosaure <kern956@gmail.com>
2023-06-12 20:55:22 +0200

    patch for the dlc unlocker after update 237

--- UltimateTrainer5/hooks/lib/managers/dlcmanager.lua
+++ UltimateTrainer5/hooks/lib/managers/dlcmanager.lua
@@ -2,12 +2,15 @@ if not UT:getSetting("enable_dlc_unlocker") then
     do return end
 end
 
-function WINDLCManager:_verify_dlcs()
-    for dlcName, dlcData in pairs(Global.dlc_manager.all_dlc_data) do
-        dlcData.verified = true
+function WinSteamDLCManager:_verify_dlcs()
+    for _, dlc_data in pairs(Global.dlc_manager.all_dlc_data) do
+    	dlc_data.verified = true
     end
 end
 
+function WinSteamDLCManager:_check_dlc_data(dlc_data)
+    return dlc_data.verified
+end
 function GenericDLCManager.has_raidww2_clan()
     return true
 end
